26f7d7e3d9c0c196d4efe00c84e596e83c4d70ae
drekain <drekainsmith@gmail.com>
2025-11-16 16:12:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo..."
-git clone https://github.com/Popeye68/TXT-EXTRACTOR
-cd /TXT-EXTRACTOR
+git clone https://github.com/drekain/TxXT-EEXTRACTORR
+cd /TxXT-EEXTRACTORR
 pip install -r requirements.txt
 echo "Starting Bot..."
 python -m Extractor
